b6db5763af674b8368f50d9a9d7497469aeccff8
Vipul Chavan <65064718+vipul-chavan@users.noreply.github.com>
2025-01-20 22:47:46 +0530

    Time: 365 ms (5.20%) | Memory: 46.3 MB (34.60%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+
+        for(int i=0;i<nums.length;i++)
+        {   
+            int num=nums[i];
+
+            int cnt=0;
+
+            for(int j=0;j<nums.length;j++)
+            {
+                if(nums[j]==num)
+                {
+                    cnt ++;
+                }
+            }
+
+            if(cnt==1)
+            {
+                return num;
+
+            }
+        }
+
+        return -1 ;
+        
+        
+    }
+}
